c6ec1f7984efd4ce14178c32d9faccc65790ab8e
Ronak Mistry <59952103+ronak007mistry@users.noreply.github.com>
2021-04-13 01:30:16 +0530

    More users in local storage

--- Local storage/main2.js
+++ Local storage/main2.js
@@ -1,6 +1,37 @@
+const form = document.querySelector('#my-form')
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+const message = document.querySelector('.msg')
+const users = document.querySelector('#users')
+const ul = document.querySelector('ul');
 
 
-// Store
-localStorage.setItem("lastname", "Smith");
-// Retrieve
-document.getElementById("users").innerHTML = localStorage.getItem("lastname");
+let itemsArray = localStorage.getItem('userDetails') ? JSON.parse(localStorage.getItem('userDetails')) : [];
+
+localStorage.setItem('userDetails', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('userDetails'));
+
+const liMaker = (text) => {
+    const li = document.createElement('li');
+    li.textContent = text;
+    ul.appendChild(li);
+}
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let userdetails = {
+        name: nameInput.value,
+        email: emailInput.value,
+    }
+
+
+    itemsArray.push(userdetails);
+    localStorage.setItem('iteuserDetailsms', JSON.stringify(itemsArray));
+    liMaker("Name: " + userdetails.name + " Email: " + userdetails.email);
+    userdetails = null;
+});
+
+data.forEach(item => {
+    liMaker(item);
+});
+
