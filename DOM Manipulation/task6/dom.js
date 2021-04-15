652daa7614710ce477104d6a3b96237ca908cb9e
Ronak Mistry <59952103+ronak007mistry@users.noreply.github.com>
2021-04-15 23:27:49 +0530

    task6

---
+++ DOM Manipulation/task6/dom.js
@@ -0,0 +1,76 @@
+
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');  
+var filter=document.getElementById('filter')  
+
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+itemList.addEventListener('click', editItem);
+filter.addEventListener('keyup',fliterItems);
+
+function addItem(e){
+  e.preventDefault();
+
+  var newItem = document.getElementById('item').value;
+  var newItemdesc = document.getElementById('desc').value;
+
+  var li = document.createElement('li');
+  
+  li.className = 'list-group-item';
+  
+  li.appendChild(document.createTextNode(`${newItem} ${newItemdesc}`));
+
+  
+  var deleteBtn = document.createElement('button');
+
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  var editBtn = document.createElement('button');
+
+  editBtn.className = 'ml-3 btn btn-success btn-sm float-right edit';
+
+  editBtn.appendChild(document.createTextNode('Edit'));
+  editBtn.contentEditable="false"
+  deleteBtn.contentEditable="false"
+  li.appendChild(editBtn);
+  li.appendChild(deleteBtn);
+  
+  itemList.appendChild(li);
+}
+
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+function editItem(e){
+    if(e.target.classList.contains('edit')){
+        var li = e.target.parentElement;
+        li.contentEditable = "true";
+      }
+  }
+
+function fliterItems(e)
+{
+  var text=e.target.value.toLowerCase();
+  var items=itemList.getElementsByTagName('li')
+  Array.from(items).forEach(function(item){
+    var itemName=item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text)!=-1)
+    {
+      item.style.display='block'
+    }
+    else
+    {
+      item.style.display='none'
+    }
+  });
+}
+
