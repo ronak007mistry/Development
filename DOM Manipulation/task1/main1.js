cab1b72465c53580451eb6d7a14f457360b3c496
Ronak Mistry <59952103+ronak007mistry@users.noreply.github.com>
2021-04-15 16:59:35 +0530

    task one

---
+++ DOM Manipulation/task1/main1.js
@@ -0,0 +1,40 @@
+// Examine the document object
+
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+
+// // document.title = 1234;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[8]);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+// GETELEMENTBYID
+
+console.log(document.getElementById('header-title'));
+
+let headerTitle = document.getElementById('header-title');
+let header = document.getElementById('main-header');
+console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+headerTitle.innerHTML = '<h3>Hello</h3>';
+
+header.style.border = 'solid 3px #000';
+
+
+// GETELEMENTSBYCLASSNAME
+
+let items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[1].textContent = 'Hello there';
+items[1].style.fontWeight = 'bold';
+items[1].style.color = 'green';
+
